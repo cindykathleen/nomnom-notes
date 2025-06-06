0b720873e0867442cb3e0bd08c832867acdde5a2
Cindy Pan <cindy.k.pan@gmail.com>
2025-06-06 16:58:19 -0700

    Implemented a preview when updating images

--- src/app/components/UploadImage.tsx
+++ src/app/components/UploadImage.tsx
@@ -7,6 +7,7 @@ interface Props {
 
 export const UploadImage: React.FC<Props> = ({ currImage, setNewImage }) => {
   const [showUploadInput, setShowUploadInput] = useState<boolean>(false);
+  const [previewImage, setPreviewImage] = useState<string>(currImage);
 
   const handleClick = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -25,22 +26,26 @@ export const UploadImage: React.FC<Props> = ({ currImage, setNewImage }) => {
         return;
       }
 
+      setPreviewImage(`/uploads/${e.target.files[0].name.replaceAll(" ", "_")}`);
       setNewImage(`/uploads/${e.target.files[0].name.replaceAll(" ", "_")}`);
     }
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       {showUploadInput
-        ? <label className="pb-1 font-semibold">Image (<span className="underline cursor-pointer hover:text-blue-900" onClick={() => setShowUploadInput(false)}>use an existing image</span>)</label>
-        : <label className="pb-1 font-semibold">Image (<span className="underline cursor-pointer hover:text-blue-900" onClick={() => setShowUploadInput(true)}>upload your own</span>)</label>
+        ? <label className="font-semibold">Image (<span className="underline cursor-pointer hover:text-blue-900" onClick={() => setShowUploadInput(false)}>use an existing image</span>)</label>
+        : <label className="font-semibold">Image (<span className="underline cursor-pointer hover:text-blue-900" onClick={() => setShowUploadInput(true)}>upload your own</span>)</label>
       }
+      {previewImage !== '' && (
+        <img src={previewImage} className="max-w-48 mb-1" />
+      )}
       {showUploadInput
         ? <input key="file-input" type="file" accept="image/*"
           className="mb-6 file:px-3 file:py-1 file:mr-2 file:font-semibold file:border file:border-blue-900 file:rounded-lg file:cursor-pointer"
           onChange={handleClick} />
         : <input key="url-input" type="text" placeholder="Add image URL here"
-          value={currImage} onChange={(e) => setNewImage(e.target.value)}
+          value={currImage} onChange={(e) => setNewImage(e.target.value)} onBlur={(e) => setPreviewImage(e.target.value)}
           className="w-full px-2 py-1 mb-6 border border-black border-solid rounded-sm focus:outline-none focus:border-blue-900 focus:shadow-(--input-shadow)" autoComplete="off" />
       }
     </div>
