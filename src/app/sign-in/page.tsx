079388bc27dac639690622ea6dba41055bfca86d
Cindy Pan <cindy.k.pan@gmail.com>
2025-09-10 16:09:29 -0700

    Show a message in the sign in and sign up page if they were redirected from an invite

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,18 +1,35 @@
+import { db } from '@/app/lib/database';
+import { User } from '@/app/interfaces/interfaces';
 import Link from 'next/link';
 import { SignInForm } from './SignInForm';
 
-export default function Page({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined }; }) {
+export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined }; }) {
   const redirect = searchParams?.redirect;
   const signUpUrl =
-    typeof redirect === 'string'
+    redirect
       ? `/sign-up?redirect=${encodeURIComponent(redirect)}`
       : '/sign-up';
 
+  let token = '';
+  let owner: User | null = null;
+
+  if (redirect) {
+    token = redirect.split("/")[2];
+    owner = await db.getOwnerByToken(token);
+  }
+  
   return (
     <div className="relative h-screen w-screen p-16 flex items-center justify-center bg-coolbeige">
       <div className="max-w-[1440px] w-full flex flex-col items-center">
         <div className="h-fit w-2xl px-12 py-10 bg-snowwhite rounded-3xl">
           <h2 className="pb-2 text-3xl font-semibold text-center">Sign in</h2>
+          { // If the user was redirected from an invitation link, show this message
+            redirect && owner && (
+              <p className="pb-2 text-md font-semibold text-center">
+                {owner.name} has invited you to collaborate on their list. Sign in to your account to accept the invitation.
+              </p>
+            )
+          }
           <p className="pb-4 text-md font-semibold text-center">
             Don't have an account? Click
             <Link href={signUpUrl} className="text-darkpink hover:text-mauve transition-colors"> here </Link>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,18 +1,35 @@
+import { db } from '@/app/lib/database';
+import { User } from '@/app/interfaces/interfaces';
 import Link from 'next/link';
 import { SignUpForm } from './SignUpForm';
 
-export default function Page({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined }; }) {
+export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined }; }) {
   const redirect = searchParams?.redirect;
   const signInUrl =
     typeof redirect === 'string'
       ? `/sign-in?redirect=${encodeURIComponent(redirect)}`
       : '/sign-in';
 
+  let token = '';
+  let owner: User | null = null;
+
+  if (redirect) {
+    token = redirect.split("/")[2];
+    owner = await db.getOwnerByToken(token);
+  }
+
   return (
     <div className="relative h-screen w-screen p-16 flex items-center justify-center bg-coolbeige">
       <div className="max-w-[1440px] w-full flex flex-col items-center">
         <div className="h-fit w-2xl px-12 py-10 bg-snowwhite rounded-3xl">
           <h2 className="pb-2 text-3xl font-semibold text-center">Create an account</h2>
+          { // If the user was redirected from an invitation link, show this message
+            redirect && owner && (
+              <p className="pb-2 text-md font-semibold text-center">
+                {owner.name} has invited you to collaborate on their list. Create an account to accept the invitation.
+              </p>
+            )
+          }
           <p className="pb-4 text-md font-semibold text-center">
             Already have an account? Click
             <Link href={signInUrl} className="text-darkpink hover:text-mauve transition-colors"> here </Link>
