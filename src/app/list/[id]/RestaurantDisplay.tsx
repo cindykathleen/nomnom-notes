d578cb28e472f25134876674648d26dda466ee09
Cindy Pan <cindy.k.pan@gmail.com>
2025-09-22 16:58:00 -0700

    Implemented the function to rate limit users on the map display feature

--- src/app/list/[id]/RestaurantDisplay.tsx
+++ src/app/list/[id]/RestaurantDisplay.tsx
@@ -12,11 +12,13 @@ enum SortType {
 export default function RestaurantDisplay({ 
   userId, 
   isOwnerOrCollaborator, 
+  featureAccessAllowed,
   list, 
   initialRestaurants 
 }: { 
   userId: string, 
   isOwnerOrCollaborator: boolean, 
+  featureAccessAllowed: boolean,
   list: List, 
   initialRestaurants: Restaurant[] 
 }) {
@@ -54,7 +56,7 @@ export default function RestaurantDisplay({
   }
 
   return (
-    <div className={`${userId === 'public' ? "w-full" : "w-1/2"} flex flex-col gap-2`}>
+    <div className={`${userId === 'public' || !featureAccessAllowed ? "w-full" : "w-1/2"} flex flex-col gap-2`}>
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-semibold">{`${restaurants.length} Places`}</h2>
         <form className="flex items-center w-fit my-2">
@@ -71,7 +73,7 @@ export default function RestaurantDisplay({
       </div>
       <div
         className={`max-h-[80vh] overflow-y-auto gap-8 
-        ${userId === "public" ? "grid grid-cols-2" : "pr-4 flex flex-col"}`}
+        ${userId === 'public' || !featureAccessAllowed ? "grid grid-cols-2" : "pr-4 flex flex-col"}`}
       >
         {restaurants.map((restaurant: Restaurant) => (
           <RestaurantCard
