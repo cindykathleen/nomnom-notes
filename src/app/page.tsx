6e1c4a7d033995d8c4f05ed7a5fe20858922deea
Cindy Pan <cindy.k.pan@gmail.com>
2025-09-23 11:27:53 -0700

    Changed homepage CTA for users vs. public users. Also fixed some search feature bugs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
+import { auth } from '@/app/lib/auth';
+import { headers } from 'next/headers';
 import Link from 'next/link';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
   return (
     <div className="relative h-screen w-screen flex flex-col items-center justify-center bg-coolbeige">
       <div className="max-w-[1440px] w-full flex flex-col items-center">
         <div className="w-1/3 text-center">
           <h1 className="text-6xl mb-6 font-semibold">NomNom Notes</h1>
           <p className="mb-6 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris elementum ipsum eget libero iaculis pretium. Sed quis lobortis sapien. Mauris diam justo, ullamcorper sed tortor sit amet, pretium ornare massa.</p>
-          <Link href="/sign-up/"
-            className="inline-block px-12 py-4 text-xl text-snowwhite font-bold bg-darkpink rounded-lg hover:bg-mauve transition-colors">
-            Get started
-          </Link>
+          { // Set CTA to lists if logged in 
+            session && (
+              <Link href="/lists"
+                className="inline-block px-12 py-4 text-xl text-snowwhite font-bold bg-darkpink rounded-lg hover:bg-mauve transition-colors">
+                View your lists
+              </Link>
+            )
+          }
+          { // Set CTA to sign up if not logged in
+            !session && (
+              <Link href="/sign-up"
+                className="inline-block px-12 py-4 text-xl text-snowwhite font-bold bg-darkpink rounded-lg hover:bg-mauve transition-colors">
+                Get started
+              </Link>
+            )
+          }
         </div>
       </div>
-      {/* <div className="absolute bottom-0">
-        <img src="homepage_laptop-screen_640x360.png" alt="Laptop screen with NomNom Notes app" width="640" height="360" />
-      </div> */}
     </div>
   );
 }
