ab703ffd42a616017e074c25e895c249e0cc62cb
Cindy Pan <cindy.k.pan@gmail.com>
2025-10-15 16:17:44 -0700

    Using an env variable for the Better Auth session token name

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get('better-auth.session_token');
+  const sessionCookie = request.cookies.get(process.env.BETTER_AUTH_SESSION_TOKEN!);
 
   if (!sessionCookie) {
     const redirectUrl = request.nextUrl.pathname + request.nextUrl.search;
