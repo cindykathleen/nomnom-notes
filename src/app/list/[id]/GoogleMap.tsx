dcea4c0a02edfc6387c32e0bf738e285fa209bd1
Cindy Pan <cindy.k.pan@gmail.com>
2025-12-10 13:02:29 -0800

    Updated Google Maps to use bounds instead of a calculated center

--- src/app/list/[id]/GoogleMap.tsx
+++ src/app/list/[id]/GoogleMap.tsx
@@ -1,20 +1,19 @@
 'use client';
 
+import { useEffect } from 'react';
 import { Restaurant } from '@/app/interfaces/interfaces';
-import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker, Pin, useMap } from '@vis.gl/react-google-maps';
 
 type Poi = {
   key: string,
   location: google.maps.LatLngLiteral
 }
 
-const PoiMarkers = (props: { pois: Poi[] }) => {
+const PoiMarkers = ({ pois }: { pois: Poi[] }) => {
   return (
     <>
-      {props.pois.map((poi: Poi) => (
-        <AdvancedMarker
-          key={poi.key}
-          position={poi.location}>
+      {pois.map((poi: Poi) => (
+        <AdvancedMarker key={poi.key} position={poi.location}>
           <Pin />
         </AdvancedMarker>
       ))}
@@ -22,20 +21,23 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
   );
 };
 
-const computeCenter = (locations: Poi[]) => {
-  const lats = locations.map((p) => p.location.lat);
-  const lngs = locations.map((p) => p.location.lng);
+const FitBoundsHandler = ({ locations }: { locations: Poi[] }) => {
+  const map = useMap();
 
-  const avgLat = lats.reduce((sum, val) => sum + val, 0) / lats.length;
-  const avgLng = lngs.reduce((sum, val) => sum + val, 0) / lngs.length;
+  useEffect(() => {
+    if (!map || locations.length === 0) return;
 
-  return { lat: avgLat, lng: avgLng };
-}
+    const bounds = new google.maps.LatLngBounds();
+    locations.forEach((poi) => bounds.extend(poi.location));
+
+    map.fitBounds(bounds);
+  }, [map, locations]);
+
+  return null;
+};
 
 export default function GoogleMap({ restaurants }: { restaurants: Restaurant[] }) {
-  if (restaurants.length === 0) {
-    return;
-  }
+  if (restaurants.length === 0) return;
 
   const locations: Poi[] = restaurants.map((restaurant) => ({
     key: restaurant._id,
@@ -45,15 +47,12 @@ export default function GoogleMap({ restaurants }: { restaurants: Restaurant[] }
     },
   }));
 
-  const center = locations.length
-    ? computeCenter(locations)
-    : { lat: 0, lng: 0 };
-
   return (
     <div className="hidden lg:block lg:w-1/2" data-cy="map-display">
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
-        <Map defaultZoom={12} defaultCenter={center} mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_ID!}>
+        <Map mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_ID!} defaultCenter={locations[0].location} defaultZoom={4}>
           <PoiMarkers pois={locations} />
+          <FitBoundsHandler locations={locations} />
         </Map>
       </APIProvider>
     </div>
