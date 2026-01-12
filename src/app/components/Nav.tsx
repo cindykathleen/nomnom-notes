45ae55ace554d00be1fef298a4ad90b08aff216d
Cindy Pan <cindy.k.pan@gmail.com>
2026-01-12 10:26:52 -0800

    Updated project logo and favicon

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Nav() {
   return (
     <div className="nav-layout">
       <div className="nav-layout-inner">
         <div>
-          <Link href="/" className="text-xl font-bold md:text-2xl">NomNom Notes</Link>
+          <Link href="/">
+            {/* Mobile logo */}
+            <Image src="/logo-mobile.png" alt="NomNom Notes logo"
+              width={80} height={51} className="block md:hidden"
+            />
+            {/* Desktop */}
+            <Image src="/logo-desktop.png" alt="NomNom Notes logo"
+              width={250} height={64} className="hidden md:block"
+            />
+          </Link>
         </div>
         <ul className="flex flex-row items-center gap-6 text-md font-semibold md:text-lg">
           <li><Link href="/lists">Lists</Link></li>
