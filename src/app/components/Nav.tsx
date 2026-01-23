3303cf638bc2a56ed95d2cabbae308490ec3e295
Cindy Pan <cindy.k.pan@gmail.com>
2026-01-23 10:16:33 -0800

    Implemented the sign out feature

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,7 +1,23 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { signOut } from '@/app/actions/authentication';
 
 export default function Nav() {
+  const router = useRouter();
+
+  const handleSignOut = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const result = await signOut();
+
+    if (result.success) {
+      router.push('/');
+    }
+  }
+
   return (
     <div className="nav-layout">
       <div className="nav-layout-inner">
@@ -20,6 +36,7 @@ export default function Nav() {
         <ul className="flex flex-row items-center gap-6 text-md font-semibold md:text-lg">
           <li><Link href="/lists">Lists</Link></li>
           <li><Link href="/search">Search</Link></li>
+          <li><Link href="#" onClick={handleSignOut}>Sign out</Link></li>
         </ul>
       </div>
     </div>
