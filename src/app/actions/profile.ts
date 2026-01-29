7e35e91a080dd5d6746c4b01dd21432d4267e148
Cindy Pan <cindy.k.pan@gmail.com>
2026-01-29 10:45:44 -0800

    Profile page reviews component

--- src/app/actions/profile.ts
+++ src/app/actions/profile.ts
@@ -1,12 +1,14 @@
 'use server';
 
-import { getListIds, getList, getRestaurant } from '@/app/lib/dbFunctions';
+import { getListIds, getList, getRestaurant, getDish } from '@/app/lib/dbFunctions';
 
 interface ProfileItem {
   _id: string;
   name: string;
-  photoUrl: string;
+  photoUrl?: string;
   dateUpdated: Date;
+  rating?: number;
+  note?: string;
 }
 
 export const getProfileLists = async (userId: string) => {
@@ -77,6 +79,63 @@ export const getProfileRestaurants = async (userId: string) => {
 
     // Only return the top 4 most recently updated restaurants
     return restaurants.slice(0, 4);
+  } catch (err) {
+    return [];
+  }
+}
+
+export const getProfileReviews = async (userId: string) => {
+  try {
+    const listIds = await getListIds(userId);
+    const restaurantIds: string[] = [];
+    const dishIds: string[] = [];
+
+    await Promise.all(
+      listIds.map(async (listId) => {
+        const list = await getList(listId);
+
+        if (!list) return null;
+
+        restaurantIds.push(...list.restaurants);
+      })
+    );
+
+    await Promise.all(
+      restaurantIds.map(async (restaurantId) => {
+        const restaurant = await getRestaurant(restaurantId);
+
+        if (!restaurant) return null;
+
+        dishIds.push(...restaurant.dishes);
+      })
+    )
+
+    const reviews = (
+      await Promise.all(
+        dishIds.map(async (dishId) => {
+          const dish = await getDish(dishId);
+
+          if (!dish) return null;
+
+          return dish.reviews.filter(review => review.createdBy === userId).map(review => {
+            return ({
+              _id: review._id,
+              name: dish.name,
+              dateUpdated: dish.dateUpdated,
+              rating: review.rating,
+              note: review.note,
+            })
+          })
+        }
+        )
+      )
+    ).flat().filter(Boolean) as ProfileItem[];
+
+    // Sort reviews by when they were last updated
+    reviews.sort((a, b) => b.dateUpdated.getTime() - a.dateUpdated.getTime());
+
+    // Only return the top 4 most recently updated reviews
+    return reviews.slice(0, 4);
   } catch (err) {
     return [];
   }
