dc788a43fd9c53670e533587408fca2adc341462
Cindy Pan <cindy.k.pan@gmail.com>
2026-01-29 13:26:25 -0800

    Settings page layout and profile component

---
+++ src/app/settings/Accordion.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { useState } from 'react';
+import { User } from '@/app/interfaces/interfaces';
+import Profile from './Profile';
+
+export default function Accordion({ user }: { user: User }) {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const accordions = [
+    {
+      title: 'Profile',
+      content: <Profile user={user} />,
+    },
+    {
+      title: 'Email',
+      content: 'hi',
+    },
+    {
+      title: 'Password',
+      content: 'it',
+    },
+    {
+      title: 'Privacy',
+      content: 'condy',
+    },
+  ]
+
+  return (
+    <div className="accordion">
+      {/* Accordion content (desktop) */}
+      <div className="accordion-title">
+        { accordions.map((accordion, index) => (
+            <button key={index} onClick={() => { setActiveIndex(index) }}
+              className={`flex-1 ${activeIndex === index ? 'active' : ''} xl:flex-0`}
+            >
+              {accordion.title}
+            </button>
+          ))
+        }
+      </div>
+      {/* Accordion content (mobile) */}
+      <select id="setting-select" className="accordion-title-select" 
+        onChange={(e) => { setActiveIndex(Number(e.target.value)) }}
+      >
+        { accordions.map((accordion, index) => (
+            <option key={index} value={index}>
+              {accordion.title}
+            </option>
+          ))
+        }
+      </select>
+      <div className="accordion-content">
+        { // Accordion content
+          accordions.map((accordion, index) => (
+            <div key={index}>
+              {
+                activeIndex === index && (
+                  <div>
+                    {accordion.content}
+                  </div>
+                )
+              }
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,31 @@
+import getCurrentUser from '@/app/lib/getCurrentUser';
+import { getUser } from '@/app/lib/dbFunctions';
+import Nav from '@/app/components/Nav';
+import Accordion from './Accordion';
+
+export default async function Page() {
+  const userId = await getCurrentUser(false);
+  const user = await getUser(userId);
+
+  return (
+    <>
+      <Nav />
+      <div className="gated-page-layout">
+        <div className="gated-page-layout-inner">
+          { user && (
+            <>
+              <h1 className="page-heading">Account settings</h1>
+              <Accordion user={user} />
+            </>
+          )}
+          { !user && (
+            <>
+              <h1 className="page-heading">Uh oh!</h1>
+              <p>We are not able to load your account information. Please try again.</p>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
