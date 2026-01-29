762d49f9ee72a70c7dbdad8f2955f7cd7eb7d5b8
Cindy Pan <cindy.k.pan@gmail.com>
2026-01-28 17:11:15 -0800

    Profile page lists component

--- src/app/profile/[id]/Photos.tsx
+++ src/app/profile/[id]/Photos.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { User } from '@/app/interfaces/interfaces';
 
 export default async function Photos({ user }: { user: User }) {
-  const randomPhotos = user.photos
+  const profilePhotos = user.photos
     .slice() // make a copy of the array
     .sort(() => 0.5 - Math.random())
     .slice(0, 9); // choose at most 9 photos
@@ -10,10 +10,10 @@ export default async function Photos({ user }: { user: User }) {
   return (
     <div className="profile-section">
       <h4 className="profile-section-heading">Photos</h4>
-      <div className="profile-section-photos">
-        {
-          randomPhotos.length > 0 && (
-            randomPhotos.map((photo, index) => {
+      { // Display photos if available
+        profilePhotos.length > 0 && (
+          <div className="profile-section-photos">
+            { profilePhotos.map((photo, index) => {
               let visibility = '';
 
               if (index >= 4 && index < 6) {
@@ -29,10 +29,15 @@ export default async function Photos({ user }: { user: User }) {
                   className={`aspect-square object-cover rounded-sm ${visibility}`}
                 />
               )
-            })
-          )
-        }
-      </div>
+            })}
+          </div>
+        )
+      }
+      { // Display an error message if no photos are found
+        profilePhotos.length === 0 && (
+          <p className="text-lg lg:text-xl">The user has not uploaded any photos.</p>
+        )
+      }
     </div>
   );
 }
