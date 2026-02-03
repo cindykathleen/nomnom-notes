f48bb9234a92d829e1a012916882eaad2b350c3c
Cindy Pan <cindy.k.pan@gmail.com>
2026-02-02 17:01:03 -0800

    Added search icon to the nav bar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -28,8 +28,15 @@ export default async function Nav({ userId }: { userId: string }) {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-6">
                 <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
               </svg>
-
-            </li></Link>
+            </li>
+          </Link>
+          <Link href="/search/">
+            <li className="w-[40px] h-[40px] flex items-center justify-center bg-snowwhite rounded-full hover:bg-[#F5F5F5] md:w-[50px] md:h-[50px]">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-6">
+                <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+              </svg>
+            </li>
+          </Link>
           <li>
             <ProfileNav user={user} />
           </li>
