74baea56bd77b56e9918a0eb36d4853f51b54aa3
Cindy Pan <cindy.k.pan@gmail.com>
2026-07-01 11:08:50 -0700

    About page hero section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import getCurrentUser from '@/app/lib/getCurrentUser';
 import PublicNav from '@/app/components/PublicNav';
 import Nav from '@/app/components/Nav';
+import Hero from './Hero';
 
 export default async function Page() {
   const userId = await getCurrentUser(true);
@@ -10,6 +11,11 @@ export default async function Page() {
       { // Display a different nav bar for public users
         userId === 'public' ? <PublicNav /> : <Nav userId={userId} />
       }
+      <div className="gated-page-layout">
+        <div className="gated-page-layout-inner">
+          <Hero />
+        </div>
+      </div>
     </>
   );
 }
