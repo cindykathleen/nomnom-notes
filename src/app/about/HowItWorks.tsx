8c3fc843b4113c2d7c4ce9ed48776f6c8b7cc365
Cindy Pan <cindy.k.pan@gmail.com>
2026-07-06 10:52:07 -0700

    About page how it works section

---
+++ src/app/about/HowItWorks.tsx
@@ -0,0 +1,9 @@
+export default function HowItWorks() {
+  return (
+    <div className="h-full">
+      <div className="grid grid-cols-3 place-content-center gap-8 xl:gap-12">
+        <h2 className="page-heading">How It Works</h2>
+      </div>
+    </div>
+  );
+}
