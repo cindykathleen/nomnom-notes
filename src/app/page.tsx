018d89cde6e87ec64dc545bb738d37e16e9b3529
Cindy Pan <cindy.k.pan@gmail.com>
2026-07-15 15:38:14 -0700

    Removed lists viewing from the homepage. Changed lists icon in nav bar to new lists page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Suspense } from 'react';
 import getCurrentUser from '@/app/lib/getCurrentUser';
 import PublicNav from '@/app/components/PublicNav';
 import Nav from '@/app/components/Nav';
 import Hero from '@/app/components/Hero';
 import Overview from '@/app/components/Overview';
 import HowItWorks from '@/app/components/HowItWorks';
-import ListsLoading from '@/app/components/ListsLoading';
-import DndWrapper from '@/app/components/DndWrapper';
-import CustomLists from '@/app/lists/CustomLists';
 
 export default async function HomePage() {
   const userId = await getCurrentUser(true);
@@ -36,13 +32,8 @@ export default async function HomePage() {
           <>
             <Nav userId={userId} />
             <div className="page-layout">
-              <div className="page-layout-inner space-y-6 xl:space-y-8">
-                <h1 className="page-heading">My lists</h1>
-                <Suspense fallback={<ListsLoading />}>
-                  <DndWrapper>
-                    <CustomLists userId={userId} />
-                  </DndWrapper>
-                </Suspense>
+              <div className="page-layout-inner">
+
               </div>
             </div>
           </>
